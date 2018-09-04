23e865fe6dc210614892804b64b0331dbc3abbe8
Jim Pick <jim@backblend.com>
2018-09-04 13:59:13 -0700

    Reduce home screen

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// TODO: register service worker:
-// import registerServiceWorker from './registerServiceWorker'
 import 'tachyons/css/tachyons.css'
 import 'tachyons-flexbox/css/tachyons-flexbox.css'
 import 'tachyons-forms/css/tachyons-forms.css'
@@ -10,4 +8,3 @@ import './index.styl'
 import App from './components/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-// registerServiceWorker()
