b259c871b2c9e5c6dfbfebcc0b6b843bf4ae8fdd
Jim Pick <jim@backblend.com>
2018-09-27 21:40:20 -0700

    Adjust polling interval

--- src/config.js
+++ src/config.js
@@ -4,10 +4,11 @@ module.exports = {
       swarm: ['/dns4/rendezvous.jimpick.com/tcp/9091/wss/p2p-websocket-star'],
       bootstrap: [
         '/dns4/ipfs.jimpick.com/tcp/9092/wss/ipfs/QmScdku7gc3VvfZZvT8kHU77bt6bnH3PnGXkyFRZ17g9EG'
-      ]
-    },
-    transport: {
-      maxThrottleDelayMS: 1000
+      ],
+      transport: {
+        // maxThrottleDelayMS: 5 * 60 * 1000 // 5 minutes
+        maxThrottleDelayMS: 60 * 1000 // 60 seconds
+      }
     }
   }
 }
