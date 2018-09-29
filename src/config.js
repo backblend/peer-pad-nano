d6ed8cd7c13b7236d53004af607dcf09c94a35dd
Jim Pick <jim@backblend.com>
2018-09-28 18:03:01 -0700

    Bump peer-star-app

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 module.exports = {
   peerStar: {
     ipfs: {
-      // swarm: ['/dns4/rendezvous.jimpick.com/tcp/9091/wss/p2p-websocket-star'], // Demo
-      swarm: ['/dns4/rendezvous.jimpick.com/tcp/9093/wss/p2p-websocket-star'], // Dev
+      swarm: ['/dns4/rendezvous.jimpick.com/tcp/9091/wss/p2p-websocket-star'], // Demo
+      // swarm: ['/dns4/rendezvous.jimpick.com/tcp/9093/wss/p2p-websocket-star'], // Dev
       bootstrap: [
         '/dns4/ipfs.jimpick.com/tcp/9092/wss/ipfs/QmScdku7gc3VvfZZvT8kHU77bt6bnH3PnGXkyFRZ17g9EG'
       ],
       transport: {
-        // maxThrottleDelayMS: 5 * 60 * 1000 // 5 minutes
-        maxThrottleDelayMS: 60 * 1000 // 60 seconds
+        maxThrottleDelayMS: 2 * 60 * 1000 // 2 minutes
+        // maxThrottleDelayMS: 60 * 1000 // 60 seconds
       }
     }
   }
